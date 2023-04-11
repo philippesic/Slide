e2b0355c51d795b7110428f5952dbd65dc375a54
Pippo Pesic <philippesic06@gmail.com>
2023-04-11 11:41:02 -0700

    Finished Level3 and made small area tweaks

--- Scripts/Level3.gd
+++ Scripts/Level3.gd
@@ -7,3 +7,7 @@ func _ready():
 	Manager.playerReference = "/root/Level" + str(Manager.levelModifier) + "/Player"
 	emit_signal("parent_ready")
 
+
+
+func _on_death_box_area_entered(area):
+	get_tree().change_scene_to_file("res://Scenes/Level3.tscn")
