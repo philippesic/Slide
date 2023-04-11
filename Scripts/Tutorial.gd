ce1888a03fb560783dc491a617f7cae712413e56
Pippo Pesic <philippesic06@gmail.com>
2023-04-11 12:40:09 -0700

    Tweaks and death box for Level0

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -12,3 +12,7 @@ func _ready():
 
 func _on_door_area_entered(area):
 	$Tips/Text/Tip9.visible = true
+
+
+func _on_death_box_area_entered(area):
+	get_tree().change_scene_to_file("res://Scenes/Level0.tscn")
